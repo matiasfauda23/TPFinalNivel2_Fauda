38bd5a7ddb84be5f14844890f005058ce4e755bc
Matias Fauda <matiasfauda@gmail.com>
2026-01-21 10:30:35 -0300

    Clase MarcaNegocio

---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+        //Metodo para listar las marcas de productos
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("Select Id, Descripcion From MARCAS");
+                datos.EjecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+    }
+}
